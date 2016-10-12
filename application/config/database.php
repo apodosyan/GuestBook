19e154175f26b14a1d4c51aa9c79879d4f920b02
apodosyan <podosyan.webpartner@gmail.com>
2016-10-12 18:33:15 +0400

    change db host name to localhost

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '54.191.186.12',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => 'T-Rad1',
 	'database' => 'TEST-HS17',
