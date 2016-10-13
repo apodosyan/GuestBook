83e908705b9e531e8e9ccd2e1e0e73075220e728
apodosyan <podosyan.webpartner@gmail.com>
2016-10-13 17:17:04 +0400

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://54.244.191.61/';
+$config['base_url'] = 'http://54.244.191.61';
 
 /*
 |--------------------------------------------------------------------------
