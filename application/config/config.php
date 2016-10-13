0a2e93835ac5510aab4804d84486ad7851ec5744
apodosyan <podosyan.webpartner@gmail.com>
2016-10-13 17:16:46 +0400

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://work17.linuxserv.net';
+$config['base_url'] = 'http://54.244.191.61/';
 
 /*
 |--------------------------------------------------------------------------
